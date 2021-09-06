2b92bb8b31e65767787b58ee65d6dc19825df39a
Michael Tran <michaelhieu@hotmail.com>
2021-09-06 17:25:48 +0200

    Added some tests and ui

--- MyApp.Tests/UnitTest1.cs
+++ MyApp.Tests/UnitTest1.cs
@@ -1,14 +1,31 @@
 using System;
 using Xunit;
+using System.IO;
 
 namespace MyApp.Tests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void isLeapYear()
         {
+            Assert.Equal(true, Program.IsLeapYear(4);
+        }
 
+        [Fact]
+
+        public void isLeapYear400() {
+            Assert.Equal(true, Program.IsLeapYear(400));
+        }
+
+        [Fact]
+        public void isNotLeapYear1700() {
+            Assert.Equal(false, Program.IsLeapYear(1700);
+        }
+
+        [Fact]
+        public void isNotLeapYear3() {
+            Assert.Equal(false, Program.IsLeapYear(3));
         }
     }
 }
--- MyApp/Program.cs
+++ MyApp/Program.cs
@@ -2,11 +2,30 @@
 
 namespace MyApp
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter a year not earlier than 1582:");
+            string input = Console.ReadLine();
+            int value = int.Parse(input);
+            if(IsLeapYear(value)) {
+                Console.WriteLine("yay");
+            } else {
+                Console.WriteLine("nay");
+            }
+        }
+
+        public static bool IsLeapYear(int year) {
+
+            if(year % 100 == 0 && year % 400 != 0) {
+                return false;
+            } else if (year % 4 == 0) {
+                return true;
+            } else {
+                return false;
+            }
         }
     }
+
 }
